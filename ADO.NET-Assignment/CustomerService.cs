95d47e14de7f9a19559607237d967f2f4b31af23
Ajith D <ajithd78564@gmail.com>
2023-09-14 12:45:13 +0530

    Customer service methods are ready

--- ADO.NET-Assignment/CustomerService.cs
+++ ADO.NET-Assignment/CustomerService.cs
@@ -36,17 +36,89 @@ namespace ADO.NET_Assignment
                 }
             }
         }
-        public static int UpdateRating(int airTripId, int rating)
+        public static int UpdateRating(int airTripId, int customerId, int rating)
         {
-            return 0;
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultDBConnection"].ConnectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_Update_Rating";
+                command.Parameters.AddWithValue("@AirTripId", airTripId);
+                command.Parameters.AddWithValue("@CustomerId", customerId);
+                command.Parameters.AddWithValue("@Rating", rating);
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("Record updated successfully!");
+                    return 1;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    return -1;
+                }
+            }
         }
         public static int CheckAvailability(string from, string to, DateTime departureDateTime)
         {
-            return 0;
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultDBConnection"].ConnectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_Check_Availability";
+                command.Parameters.AddWithValue("@From", from);
+                command.Parameters.AddWithValue("@To", to);
+                command.Parameters.AddWithValue("@DepartureDateTime", departureDateTime);
+                SqlParameter rowCount = new SqlParameter("@rowCount", SqlDbType.Int);
+                rowCount.Direction = ParameterDirection.Output;
+                command.Parameters.Add(rowCount);
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    if ((int)rowCount.Value > 0)
+                        return (int)rowCount.Value;
+                    else
+                        return -1;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    return -1;
+                }
+            }
         }
-        public static int FetchAirlinesRating(int airTripId)
+        public static int? FetchAirlinesRating(int airTripId)
         {
-            return 0;
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultDBConnection"].ConnectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_Get_Rating";
+                command.Parameters.AddWithValue("@AirTripId", airTripId);
+                SqlParameter rating = new SqlParameter("@Rating", SqlDbType.Int);
+                rating.Direction = ParameterDirection.Output;
+                command.Parameters.Add(rating);
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    if (DBNull.Value.Equals(rating.Value))
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        return (int)rating.Value;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    return null;
+                }
+            }
         }
     }
 }
--- ADO.NET-Assignment/Program.cs
+++ ADO.NET-Assignment/Program.cs
@@ -10,9 +10,14 @@ namespace ADO.NET_Assignment
     {
         public static void Main(string[] args)
         {
-            //AirTrip.AddNewAirTrip("airLines1", 10, DateTime.Now, "TVM", "Ernakulam");
+            //AirTrip.AddNewAirTrip("airLines2", 20, DateTime.Now, "Dubai", "Ernakulam");
             //AirTrip.UpdateAvailability(10001, 9);
-            //CustomerService.BookTicket("customerName", "1234567890", "mail@mail.com", 10001);
+            //CustomerService.BookTicket("customerNamettt", "1636567890", "dadds@mail.com", 10002);
+            //CustomerService.UpdateRating(10001, 28, 5);
+            //int availability = CustomerService.CheckAvailability("TVM", "Ernakulam", DateTime.Parse("2023-09-14 00:53:18.620"));
+            //Console.WriteLine(availability);
+            var rating = CustomerService.FetchAirlinesRating(10001);
+            Console.WriteLine(rating);
         }
     }
 }
